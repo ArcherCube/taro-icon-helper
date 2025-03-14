6aa598316dd4afe9a8ec847e77f75172c1330be8
ArcherCube <archercube@163.com>
2025-03-14 11:14:00 +0800

    feat: 优化svg解析，完善兼容问题

--- src/generate/generate-components/task/generate-icon-component/generate-comment/convert-source.ts
+++ src/generate/generate-components/task/generate-icon-component/generate-comment/convert-source.ts
@@ -1,31 +1,98 @@
-export type ConvertSvgSourceOptions = {
-  color?: string[] | string;
+type ConvertSvgSourceOptions = {
+  color: string[] | string;
+  size?: {
+    width: number;
+    height: number;
+  };
 };
 
-const PATH_REGEXP = /<path[^<>]+>/g;
-const PATH_FILL_REPLACE_REGEXP = /fill=["'][^>"']*["']/g;
-const PATH_STROKE_REPLACE_REGEXP = /stroke=["'][^>"']*["']/g;
+type PathAttribute = {
+  stroke?: string;
+  fill?: string;
+};
 
-export const convertSvgSource = (source: string, options?: ConvertSvgSourceOptions) => {
+type SVGAttribute = {
+  xmlns?: string;
+  width?: string;
+  height?: string;
+} & PathAttribute;
+
+const SVG_ATTRIBUTE_REGEXP = /(?<=\<svg )[^\>]*(?=\>)/g;
+const PATH_ATTRIBUTE_REGEXP = /(?<=\<path )[^\>\/]*(?=\/?\>)/g;
+
+const stringToAttribute = (input: string) => {
+  return input
+    .trim()
+    .split(/(?<=\") /)
+    .map((attributeExp) => attributeExp.split('='))
+    .reduce(
+      (result, current) => {
+        const [key, wrapValue] = current;
+        const value = wrapValue.slice(1, wrapValue.length - 1);
+        if (key && value) {
+          result[key] = `${value}`;
+        }
+        return result;
+      },
+      {} as Record<string, string>,
+    );
+};
+
+const attributeToString = (attribute: Record<string, string>) => {
+  return Object.entries(attribute)
+    .map(([key, value]) => `${key}="${value}"`)
+    .join(' ');
+};
+
+const isVisibleColor = (color: string) => {
+  return !['none', 'transparent'].includes(color);
+};
+
+export const convertSvgSource = (source: string, option: ConvertSvgSourceOptions) => {
   let result = source;
-  if (options) {
-    if (options.color) {
-      const colors = typeof options.color === 'string' ? [options.color] : options.color;
-      if (colors[0]) {
-        let index = 0;
-        result = result.replaceAll(PATH_REGEXP, (pathTag) => {
-          let replacePathTag = pathTag.replaceAll(PATH_FILL_REPLACE_REGEXP, () => {
-            return `fill="${colors[index % colors.length]}"`;
-          });
-          replacePathTag = replacePathTag.replaceAll(PATH_STROKE_REPLACE_REGEXP, () => {
-            return `stroke="${colors[index % colors.length]}"`;
-          });
-          ++index;
-          return replacePathTag;
-        });
+  let svgAttribute: SVGAttribute;
+
+  // svg附加size
+  result = result.replaceAll(SVG_ATTRIBUTE_REGEXP, (attributeStr) => {
+    svgAttribute = stringToAttribute(attributeStr) as SVGAttribute;
+
+    if (option.size) {
+      svgAttribute.width = `${option.size.width}`;
+      svgAttribute.height = `${option.size.height}`;
+    }
+
+    return attributeToString({
+      ...svgAttribute,
+      // 去除stroke和fill，由每个path去设置
+      stroke: 'none',
+      fill: 'none',
+    });
+  });
+
+  // path 按顺序修改stroke和fill
+  let pathIndex = 0;
+  const color = typeof option.color === 'string' ? [option.color] : option.color;
+  result = result.replaceAll(PATH_ATTRIBUTE_REGEXP, (attributeStr) => {
+    const pathAttribute = stringToAttribute(attributeStr) as PathAttribute;
+
+    const currentColor = color[pathIndex++ % color.length];
+
+    if (pathAttribute.fill) {
+      if (isVisibleColor(pathAttribute.fill)) {
+        pathAttribute.fill = `${currentColor}`;
+      }
+    } else {
+      // path的fill为空时，若svg的fill为空（即默认的black）或有可见的颜色，则path也为这个颜色
+      if (!svgAttribute.fill || isVisibleColor(svgAttribute.fill)) {
+        pathAttribute.fill = `${currentColor}`;
       }
     }
-  }
+    if (pathAttribute.stroke && isVisibleColor(pathAttribute.stroke)) {
+      pathAttribute.stroke = `${currentColor}`;
+    }
+
+    return attributeToString(pathAttribute);
+  });
 
   return result;
 };
